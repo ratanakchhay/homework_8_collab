541f867bbd1d6709e96791ac2c1ea9791cf2e448
mengkheang-seng <2025422seng@aupp.edu.kh>
2025-07-06 17:33:13 +0700

    finished

--- exercise-3.py
+++ exercise-3.py
@@ -1,2 +1,10 @@
 def remove_all_after(numbers, n):
-    ...
+#create a new list to store the result
+#if the number is greater than n, stop adding to the result
+#if the number is equal to n, break the loop 
+    result = []
+    for i in numbers:
+        result.append(i)
+        if i == n:
+            break
+    return result
